5579b262c08c5ccee605832bc07d926bceb21700
T. Korn <tobias.korn@towerswatson.com>
2026-05-20 09:16:15 +0200

    Copied newly generated ASP .NET10 Template over old version

--- dotnet/src/AspNetCoreTemplate/Program.cs
+++ dotnet/src/AspNetCoreTemplate/Program.cs
@@ -1,19 +1,33 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Hosting;
 
-namespace TemplateNamespace {
+namespace AspNetCoreTemplate {
 
   public class Program {
 
     public static void Main(string[] args) {
-      CreateHostBuilder(args).Build().Run();
-    }
 
-    public static IHostBuilder CreateHostBuilder(string[] args) =>
-      Host.CreateDefaultBuilder(args).ConfigureWebHostDefaults(webBuilder => {
-        webBuilder.UseStartup<Startup>();
+      WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+      // Add services to the container.
+      builder.Services.AddControllers();
+
+      // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+      builder.Services.AddOpenApi();
+
+      WebApplication app = builder.Build();
+
+      // Configure the HTTP request pipeline.
+      if (app.Environment.IsDevelopment()) {
+        app.MapOpenApi();
       }
-    );
+
+      app.UseHttpsRedirection();
+
+      app.UseAuthorization();
+
+      app.MapControllers();
+
+      app.Run();
+    }
 
   }
 
--- dotnet/src/AspNetCoreTemplate/Startup.cs
+++ dotnet/src/AspNetCoreTemplate/Startup.cs
@@ -1,128 +1,128 @@
-using Logging.SmartStandards.AspSupport;
-using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Web.UJMW;
+//using Logging.SmartStandards.AspSupport;
+//using Microsoft.AspNetCore;
+//using Microsoft.AspNetCore.Builder;
+//using Microsoft.AspNetCore.Hosting;
+//using Microsoft.Extensions.Configuration;
+//using Microsoft.Extensions.DependencyInjection;
+//using Microsoft.Extensions.Hosting;
+//using Microsoft.Extensions.Logging;
+//using System;
+//using System.Web.UJMW;
 
 
-namespace TemplateNamespace {
+//namespace TemplateNamespace {
 
-  public class Startup {
+//  public class Startup {
 
-    public Startup(IConfiguration configuration) {
-      _Configuration = configuration;
-    }
+//    public Startup(IConfiguration configuration) {
+//      _Configuration = configuration;
+//    }
 
-    private static SmartStandardsCheapInitializer _AnyIocInitHelper = new SmartStandardsCheapInitializer();
+//    private static SmartStandardsCheapInitializer _AnyIocInitHelper = new SmartStandardsCheapInitializer();
 
-    private static IConfiguration _Configuration = null;
+//    private static IConfiguration _Configuration = null;
 
-    public void ConfigureServices(IServiceCollection services) {
+//    public void ConfigureServices(IServiceCollection services) {
 
-      _AnyIocInitHelper.ConfigureServices(_Configuration, services);
+//      _AnyIocInitHelper.ConfigureServices(_Configuration, services);
 
-      //services.AddLogging();
-      services.AddSmartStandardsLogging(_Configuration);
+//      //services.AddLogging();
+//      services.AddSmartStandardsLogging(_Configuration);
 
-      string outDir = AppDomain.CurrentDomain.BaseDirectory;
+//      string outDir = AppDomain.CurrentDomain.BaseDirectory;
 
-      services.AddControllers();
+//      services.AddControllers();
  
-      MyDemoService myDemoService = new MyDemoService();
-      services.AddSingleton<IMyDemoService>(myDemoService);
+//      MyDemoService myDemoService = new MyDemoService();
+//      services.AddSingleton<IMyDemoService>(myDemoService);
 
-      UjmwHostConfiguration.UseCombinedDynamicAssembly = true;
-      services.AddDynamicUjmwControllers((r) => {
+//      UjmwHostConfiguration.UseCombinedDynamicAssembly = true;
+//      services.AddDynamicUjmwControllers((r) => {
 
-        r.AddControllerFor<IMyDemoService>((options) => {
-        });
+//        r.AddControllerFor<IMyDemoService>((options) => {
+//        });
   
-      });
+//      });
       
-      //services.AddSwaggerGenSmartStandardsFlavored();
-    }
+//      //services.AddSwaggerGenSmartStandardsFlavored();
+//    }
 
-    public void Configure(
-      IApplicationBuilder app, IWebHostEnvironment env,
-      ILoggerFactory loggerfactory, IHostApplicationLifetime lifetimeEvents
-    ) {
+//    public void Configure(
+//      IApplicationBuilder app, IWebHostEnvironment env,
+//      ILoggerFactory loggerfactory, IHostApplicationLifetime lifetimeEvents
+//    ) {
 
-      _AnyIocInitHelper.Configure(app, env, loggerfactory, lifetimeEvents);
+//      _AnyIocInitHelper.Configure(app, env, loggerfactory, lifetimeEvents);
 
-      //required for the www-root
-      app.UseStaticFiles();
+//      //required for the www-root
+//      app.UseStaticFiles();
 
-      app.UseAmbientFieldAdapterMiddleware();
+//      app.UseAmbientFieldAdapterMiddleware();
 
-      if (!_Configuration.GetValue<bool>("ProdMode")) {
-        app.UseDeveloperExceptionPage();
-      }
+//      if (!_Configuration.GetValue<bool>("ProdMode")) {
+//        app.UseDeveloperExceptionPage();
+//      }
 
-      string baseUrl = _Configuration.GetValue<string>("BaseUrl");
+//      string baseUrl = _Configuration.GetValue<string>("BaseUrl");
  
-      app.UseHttpsRedirection();
+//      app.UseHttpsRedirection();
 
-      app.UseRouting();
+//      app.UseRouting();
 
-      //CORS: muss zwischen 'UseRouting' und 'UseEndpoints' liegen!
-      app.UseCors(p =>
-          p.AllowAnyOrigin()
-          .AllowAnyMethod()
-          .AllowAnyHeader()
-      );
+//      //CORS: muss zwischen 'UseRouting' und 'UseEndpoints' liegen!
+//      app.UseCors(p =>
+//          p.AllowAnyOrigin()
+//          .AllowAnyMethod()
+//          .AllowAnyHeader()
+//      );
 
-      app.UseAuthentication(); //<< WINDOWS-AUTH
-      app.UseAuthorization();
+//      app.UseAuthentication(); //<< WINDOWS-AUTH
+//      app.UseAuthorization();
 
-      app.UseEndpoints(endpoints => {
-        endpoints.MapControllers();
-      });
+//      app.UseEndpoints(endpoints => {
+//        endpoints.MapControllers();
+//      });
 
-      //SelfAnnouncementHelper.Configure(
-      //  lifetimeEvents, app.ServerFeatures,
-      //  (string[] baseUrls, EndpointInfo[] endpoints, bool act, ref string info) => {
+//      //SelfAnnouncementHelper.Configure(
+//      //  lifetimeEvents, app.ServerFeatures,
+//      //  (string[] baseUrls, EndpointInfo[] endpoints, bool act, ref string info) => {
 
-      //    var sb = new StringBuilder();
-      //    string timestamp = DateTime.Now.ToLongTimeString();
+//      //    var sb = new StringBuilder();
+//      //    string timestamp = DateTime.Now.ToLongTimeString();
 
-      //    Console.WriteLine("--------------------------------------");
-      //    if (act) {
-      //      Console.WriteLine("ANNOUNCE:");
-      //    }
-      //    else {
-      //      Console.WriteLine("UN-ANNOUNCE:");
-      //    }
-      //    Console.WriteLine("--------------------------------------");
-      //    foreach (EndpointInfo ep in endpoints) {
-      //      foreach (string url in baseUrls) {
-      //        Console.WriteLine(ep.ToString(url));
-      //        sb.Append(ep.ToString(url));
-      //        if (act) {
-      //          sb.AppendLine(" >> ONLINE @" + timestamp);
-      //        }
-      //        else {
-      //          sb.AppendLine(" >> offline @" + timestamp);
-      //        }
+//      //    Console.WriteLine("--------------------------------------");
+//      //    if (act) {
+//      //      Console.WriteLine("ANNOUNCE:");
+//      //    }
+//      //    else {
+//      //      Console.WriteLine("UN-ANNOUNCE:");
+//      //    }
+//      //    Console.WriteLine("--------------------------------------");
+//      //    foreach (EndpointInfo ep in endpoints) {
+//      //      foreach (string url in baseUrls) {
+//      //        Console.WriteLine(ep.ToString(url));
+//      //        sb.Append(ep.ToString(url));
+//      //        if (act) {
+//      //          sb.AppendLine(" >> ONLINE @" + timestamp);
+//      //        }
+//      //        else {
+//      //          sb.AppendLine(" >> offline @" + timestamp);
+//      //        }
 
-      //      }
-      //    }
-      //    Console.WriteLine("--------------------------------------");
+//      //      }
+//      //    }
+//      //    Console.WriteLine("--------------------------------------");
 
-      //    File.WriteAllText("_AnnouncementInfo.txt", sb.ToString());
+//      //    File.WriteAllText("_AnnouncementInfo.txt", sb.ToString());
 
-      //    info = "was additionally written into file '_AnnouncementInfo.txt'";
+//      //    info = "was additionally written into file '_AnnouncementInfo.txt'";
 
-      //  },
-      //  autoTriggerInterval: 1
-      //);
+//      //  },
+//      //  autoTriggerInterval: 1
+//      //);
 
-    }
+//    }
 
-  }
+//  }
 
-}
+//}
